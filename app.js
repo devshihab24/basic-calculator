ca1def2bdf3289fb9dae523648f0138d38ea0129
shihabbabu420 <mdshihabbabu478@gmail.com>
2025-09-13 17:05:26 +0600

    calcutator is ready

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-let input = document.getElementById("input");
-let buttons = document.querySelectorAll("button");
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+let string = "";
 
-let string = ''
-let arr = Array.from(buttons);
-arr.forEach(function (buttons){
-    buttons.addEventListener("click", function (event){
-        if(event.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(event.target.innerHTML == 'AC'){
-            string = '';
-            input.value = string;
-        }
-        else if(event.target.innerHTML == "DEL"){
-            string = string.substring(0, string.length-1)
-            input.value = string;
-        }
-        else{
-            string += event.target.innerHTML;
-            input.value = string;
-        }
-    })
-});
+let buttonArray = Array.from(buttons);
+for (const button of buttonArray) {
+  button.addEventListener("click", function (event) {
+    if (event.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (event.target.innerHTML == "AC") {
+      input.value = "";
+    } else if (event.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += event.target.innerHTML;
+      input.value = string;
+    }
+  });
+}
