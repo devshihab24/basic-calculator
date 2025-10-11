ba847f9d471acbac4a0c77b799a27d1de09fae0d
shihabbabu420 <mdshihabbabu478@gmail.com>
2025-10-11 12:33:52 +0600

    error fixed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const input = document.getElementById("input");
-const buttons = document.querySelectorAll("button");
+const buttons = document.getElementsByClassName("btn");
 let string = "";
+// console.log(buttons)
 
-let buttonArray = Array.from(buttons);
-for (const button of buttonArray) {
+for (const button of buttons) {
   button.addEventListener("click", function (event) {
     if (event.target.innerHTML == "=") {
+      // console.log(string)
       string = eval(string);
       input.value = string;
     } else if (event.target.innerHTML == "AC") {
